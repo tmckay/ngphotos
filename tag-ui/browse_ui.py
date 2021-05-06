fcb1786dd758744d4057abcdf8af2202cb824ec3
Taylor McKay <taylormckay@gmail.com>
2021-05-05 22:20:20 -0700

    Add basic widget to browse UI

--- tag-ui/browse_ui.py
+++ tag-ui/browse_ui.py
@@ -2,13 +2,24 @@ import sys
 
 from PyQt6.QtWidgets import (
     QApplication,
+    QLabel,
     QMainWindow
 )
+
+from PyQt6.QtCore import Qt
 
 
 class Browse(QMainWindow):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+
+        self.setWindowTitle('ngphotos :: browse')
+
+        label = QLabel('ngphotos')
+
+        label.setAlignment(Qt.Alignment.AlignCenter)
+
+        self.setCentralWidget(label)
 
 
 if __name__ == '__main__':
