d95b11cf51d45f00ec99c58870349d859821d65d
Taylor McKay <taylormckay@gmail.com>
2021-05-08 21:22:09 -0700

    Add file scanning to browse UI

--- tag-ui/browse_ui.py
+++ tag-ui/browse_ui.py
@@ -1,3 +1,6 @@
+import multiprocessing as mp
+from multiprocessing import Process, Queue
+import os
 import pathlib
 import sys
 
@@ -15,6 +18,32 @@ from PyQt6.QtCore import Qt
 from PyQt6.QtGui import QAction
 
 
+class FileScanner:
+    def __init__(self, search_dir, extensions):
+        print('init scanner')
+        self.ctx = mp.get_context('spawn')
+        self.queue = self.ctx.Queue(1)
+        self.search_dir = search_dir
+        self.extensions = extensions
+
+    @staticmethod
+    def _scan(search_dir, extensions, queue):
+        """Scans a directory"""
+        for item in os.listdir(search_dir):
+            print(f'scanning {item}')
+            path = os.path.join(search_dir, item)
+            if os.path.isfile(path):
+                ext = os.path.splitext(path)[1].lower()
+                if ext in extensions:
+                    queue.put(path)
+
+    def start(self):
+        """Starts separate process"""
+        print('start scanner')
+        self.process = self.ctx.Process(target=self._scan, args=(self.search_dir, self.extensions, self.queue,), daemon=True)
+        self.process.start()
+
+
 class BrowseWidget(QWidget):
     def __init__(self):
         super().__init__()
@@ -24,8 +53,18 @@ class BrowseWidget(QWidget):
 
         self.path_edit = QLineEdit()
         self.layout.addWidget(self.path_edit, 0, 0)
+        self.layout.setAlignment(self.path_edit, Qt.Alignment.AlignTop)
         self.path_edit.setText(str(pathlib.Path.home()))
         self.path_edit.setAlignment(Qt.Alignment.AlignTop)
+
+        self._start_scanning()
+
+    def _start_scanning(self):
+        self.scanner = FileScanner(self.path_edit.text(), ['jpg', 'jpeg', 'png'])
+        self.scanner.start()
+
+        #while not self.scanner.queue.empty():
+        #    self.statusBar().showMessage(self.scanner.queue.get())
 
 
 class Browse(QMainWindow):
