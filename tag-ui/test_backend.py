efd4463a3c72c72d7d631e5ea7b57f1950bc86e1
Taylor McKay <taylormckay@gmail.com>
2021-06-01 21:47:46 -0700

    Add test for adding images

--- tag-ui/test_backend.py
+++ tag-ui/test_backend.py
@@ -6,3 +6,12 @@ def test_create_tables():
     be.create_table()
     be.cur.execute('''SELECT count(*) FROM sqlite_master WHERE type = 'table' ''')
     assert be.cur.fetchall()[0][0] == 3
+
+def test_add_image():
+    be = Backend(':memory:')
+    be.create_table()
+    be.add_image('/Users/foo/images/bar.jpg', '123abc')
+    be.cur.execute('''SELECT count(*) FROM images''')
+    assert be.cur.fetchall()[0][0] == 1
+
+
