cb7e167a24e93822648e068c12a2adc36cf30092
Taylor McKay <taylormckay@gmail.com>
2021-06-03 22:11:28 -0700

    Add more tests for tag parsing and storing tags

--- tag-ui/test_backend.py
+++ tag-ui/test_backend.py
@@ -1,4 +1,14 @@
-from backend import Backend
+from backend import Backend, Tags
+
+
+def test_parse_tags():
+    raw_tags = 'fee fi fo fum'
+    parsed_tags = Tags.parse(raw_tags)
+    assert ['fee', 'fi', 'fo', 'fum'] == sorted(parsed_tags)
+
+    raw_tags = '"hello world" beach'
+    parsed_tags = Tags.parse(raw_tags)
+    assert ['beach', 'hello world'] == sorted(parsed_tags)
 
 
 def test_create_tables():
@@ -15,10 +25,11 @@ def test_add_image():
     assert be.cur.fetchall()[0][0] == 1
 
 def test_add_image_with_tags():
+    tags_to_add = 'fee fi fo fum'
     be = Backend(':memory:')
     be.create_table()
     be.add_image('/Users/foo/images/bar.jpg', '123abc')
-    be.update_tags('fee fi fo fum')
+    be.update_tags(tags_to_add)
 
     be.cur.execute('''SELECT count(*) FROM tags''')
     assert be.cur.fetchall()[0][0] == 4
@@ -26,3 +37,6 @@ def test_add_image_with_tags():
     be.cur.execute('''SELECT count(*) FROM image_tags''')
     assert be.cur.fetchall()[0][0] == 4
 
+    be.cur.execute('''SELECT * FROM tags''')
+    tags_added = [row[1] for row in be.cur.fetchall()]
+    assert sorted(tags_added) == ['fee', 'fi', 'fo', 'fum'] 
